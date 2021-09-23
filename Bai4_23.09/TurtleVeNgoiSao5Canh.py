e19e3543b5ff2747ac4c41a7613a19ca319b5604
vuduykhuong1412 <khuongvd.brainos@gmail.com>
2021-09-23 21:39:22 +0700

    Bai thuc hanh 4

---
+++ Bai4_23.09/TurtleVeNgoiSao5Canh.py
@@ -0,0 +1,10 @@
+#Bai ve ngoi sao nam canh su dung turtle
+from turtle import *
+import turtle
+
+star = Turtle()
+for i in range(50):
+    star.forward(50)
+    star.right(144)
+    
+turtle.done()
