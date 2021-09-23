62ac447f42e0df4f9bbaf94666237f224103e15b
cuomanhqnam <90971080+cuomanhqnam@users.noreply.github.com>
2021-09-23 22:25:36 +0700

    Bai tap 4 Nguyen Xuan Tien

---
+++ Bai4_23.09/Nguyen Xuan Tien/Caclenhchuyendong.py
@@ -0,0 +1,9 @@
+from turtle import*
+turtle = Turtle()
+turtle.pensize(5)
+turtle.pencolor("Red")
+turtle.fillcolor("Blue")
+turtle.begin_fill()
+turtle.circle(100)
+turtle.end_fill()
+Turtle.done()
---
+++ Bai4_23.09/Nguyen Xuan Tien/DemoThuvienturtle.py
@@ -0,0 +1,12 @@
+# Bước 1: Import thư viện Turtle
+from turtle import *
+
+#Bước 2: Khởi tạo
+turtle = Turtle()
+
+# Bước 3: Thực hiện vẽ về phía trước 100 Pixel
+turtle.forward(100)
+turtle.done()
+
+
+
